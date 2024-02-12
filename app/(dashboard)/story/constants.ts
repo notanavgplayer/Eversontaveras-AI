7cf0ccd743a8aa2e03ffdbb92aceb8d901186617
Asar Ahmed (Full Stack devleoper) <themernist@gmail.com>
2024-02-12 20:43:53 +0500

    declare constants for story

---
+++ app/(dashboard)/story/constants.ts
@@ -0,0 +1,60 @@
+import * as z from "zod";
+
+// Custom validation function for word count
+const wordCount = (value: string): boolean => {
+  const wordCount = value.trim().split(/\s+/).length;
+  return wordCount >= 3 && wordCount <= 2000;
+};
+
+// Custom error message for word count
+const wordCountErrorMessage = "Word Count should be between 3 and 2000 words.";
+
+export const formSchema = z.object({
+  topic: z.string().min(1, {
+    message: "Topic is required.",
+  }),
+  word_count: z.string().refine((value) => wordCount(value), {
+    message: wordCountErrorMessage,
+  }),
+  writing_mode: z.string().min(1),
+  story_genre: z.string().min(1),
+});
+
+export const writingModeOptions = [
+  {
+    value: "Standard",
+    label: "Standard",
+  },
+  {
+    value: "Complex",
+    label: "Complex",
+  },
+  {
+    value: "Creative",
+    label: "Creative",
+  },
+  {
+    value: "Simple",
+    label: "Simple",
+  },
+];
+
+
+export const storyGenreOptions = [
+  {
+    value: "science fiction",
+    label: "Science Fiction",
+  },
+  {
+    value: "mystery",
+    label: "Mystery",
+  },
+  {
+    value: "fantasy",
+    label: "Fantasy",
+  },
+  {
+    value: "descriptive",
+    label: "Descriptive",
+  },
+];
