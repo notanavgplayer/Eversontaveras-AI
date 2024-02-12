150a61edc6e5eed5900e76b555b02e26c2b354ae
Asar Ahmed (Full Stack devleoper) <themernist@gmail.com>
2024-02-12 17:02:53 +0500

    add animation on chat welcome

--- app/(dashboard)/chat/_components/chat-welcome.tsx
+++ app/(dashboard)/chat/_components/chat-welcome.tsx
@@ -1,15 +1,23 @@
-import React from 'react'
+"use client";
+import Aos from "aos";
+import React, { useEffect } from "react";
 
 const ChatWelcome = () => {
+  useEffect(() => {
+    Aos.init({
+      duration: 1000,
+    });
+  }, []);
+
   return (
-    <div className='py-8 lg:py-32'>
+    <div data-aos="fade-up" className="py-8 lg:py-32">
       <h1 className="text-2xl font-bold mb-4">Welcome to ChatBlast</h1>
-      <p className="text-sm">
+      <p className="text-sm tracking-wide">
         Ready to get started? Type your first message below and let the
         conversation begin!
       </p>
     </div>
   );
-}
+};
 
-export default ChatWelcome
+export default ChatWelcome;
